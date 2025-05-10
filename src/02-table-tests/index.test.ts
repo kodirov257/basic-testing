13f25f9e3c5cab76d5d614679aadec49d6cf4b13
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2025-05-10 23:59:14 +0500

    test: implement table tests for simple calculator

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,67 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  {
+    name: 'should add two numbers',
+    a: 1,
+    b: 2,
+    action: Action.Add,
+    expected: 3,
+  },
+  {
+    name: 'should subtract two numbers',
+    a: 6,
+    b: 2,
+    action: Action.Subtract,
+    expected: 4,
+  },
+  {
+    name: 'should multiply two numbers',
+    a: 3,
+    b: 2,
+    action: Action.Multiply,
+    expected: 6,
+  },
+  {
+    name: 'should divide two numbers',
+    a: 75,
+    b: 15,
+    action: Action.Divide,
+    expected: 5,
+  },
+  {
+    name: 'should exponentiate two numbers',
+    a: 4,
+    b: 5,
+    action: Action.Exponentiate,
+    expected: 1024,
+  },
+  {
+    name: 'should return null for invalid action',
+    a: 3,
+    b: 2,
+    action: 'invalid-action',
+    expected: null,
+  },
+  {
+    name: 'should return null for invalid arguments',
+    a: 'invalid-argument',
+    b: 2,
+    action: Action.Multiply,
+    expected: null,
+  },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  testCases.forEach((testCase) => {
+    test(testCase.name, () => {
+      const result = simpleCalculator({
+        a: testCase.a,
+        b: testCase.b,
+        action: testCase.action,
+      });
+      expect(result).toBe(testCase.expected);
+    });
   });
-  // Consider to use Jest table tests API to test all cases above
 });
